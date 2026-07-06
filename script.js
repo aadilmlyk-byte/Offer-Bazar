0178f9f5f9d7cee29477478d178d1589f6cad0f6
aadilmlyk-byte <aadilmlyk@gmail.com>
2026-07-06 21:01:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function buyNow(product, price) {
-  localStorage.setItem("product", product);
-  localStorage.setItem("price", price);
-  window.location.href = "checkout.html";
+function buyNow(productName, price) {
+    localStorage.setItem("productName", productName);
+    localStorage.setItem("productPrice", price);
+    window.location.href = "checkout.html";
 }
 
 function searchProducts() {
-  let input = document.getElementById("search").value.toLowerCase();
-  let cards = document.getElementsByClassName("product-card");
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.getElementsByClassName("product-card");
 
-  for (let i = 0; i < cards.length; i++) {
-    let title = cards[i].querySelector("h3").innerText.toLowerCase();
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].querySelector("h3").innerText.toLowerCase();
 
-    if (title.includes(input)) {
-      cards[i].style.display = "block";
-    } else {
-      cards[i].style.display = "none";
+        if (title.includes(input)) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
     }
-  }
 }
